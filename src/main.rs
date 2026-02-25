1cf394c075fab8a46a2186c1b94b8f5284ee12c7
Marc-André Landry <1249166+LMA1980@users.noreply.github.com>
2026-02-25 01:13:40 -0500

    Improved further the refactoring. Introduced unittests testing. Next steps will be benchmarks.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use rustup_configurator::target;
+use regex::Regex;
+use rustup_configurator::target::{self, Target};
 use std::{env::{self, set_current_dir},path::PathBuf,process::{Command, ExitStatus}};
 
 fn set_workspace(location: PathBuf) {
@@ -21,15 +22,17 @@ fn execute_clippy() {
 fn with_emsdk() -> (String, String, String) {
     let try_read_env_emsdk = env::var("EMSDK").unwrap_or("/opt/emsdk".to_string()).to_owned();
     let with_env_emsdk = try_read_env_emsdk.as_str();
+    #[cfg(debug_assertions)]
     dbg!(&with_env_emsdk);
     let with_env_emsdk_path = format!(
         "{with_env_emsdk}:{with_env_emsdk}/upstream/emscripten:{with_env_emsdk}/node/22.16.0_64bit/bin:/home/lma1980/.wasmtime/bin:/home/lma1980/.cargo/bin:/usr/lib64/ccache:/home/lma1980/.local/bin:/home/lma1980/bin:/usr/local/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/opt/wasi-sdk"
     );
+    #[cfg(debug_assertions)]
     dbg!(&with_env_emsdk_path);
     let with_env_emsdk_node = env::var("EMSDK_NODE").unwrap_or(format!("{with_env_emsdk}/node/22.16.0_64bit/bin/node")).to_owned();
+    #[cfg(debug_assertions)]
     dbg!(&with_env_emsdk_node);
-    ( with_env_emsdk.to_owned(), with_env_emsdk_path.to_owned(), with_env_emsdk_node.to_owned() )
-    
+    ( with_env_emsdk.to_owned(), with_env_emsdk_path.to_owned(), with_env_emsdk_node.to_owned() )  
 }
 
 fn execute_build_with_emsdk(target: &str, is_release: bool) -> ExitStatus {
@@ -69,47 +72,96 @@ fn execute_build_default(target: &str, is_release: bool) -> ExitStatus {
     cmd.status().expect("Failed to cross-build against msvc")
 }
 
+fn build(targets: &[Target], is_release: bool) {
+    let mut status;
+    for target in targets.iter() {
+        if !is_release {
+            println!("-=====================- {} [debug build] -=====================-", &target.triple);
+        } else {
+            println!("-=====================- {} [release build] -=====================-", &target.triple);
+        }
+
+        status = match target.triple.as_str() {
+            "wasm32-unknown-emscripten" => {
+                execute_build_with_emsdk(target.triple.as_str(), is_release)
+            }
+            "x86_64-pc-windows-msvc" => {
+                execute_build_with_xwin(target.triple.as_str(), is_release)
+            }
+            _ => {
+                execute_build_default(target.triple.as_str(), is_release)
+            }
+        };
+        if !status.success() {
+            std::process::exit(1);
+        }
+    }
+}
+
+fn execute_test_for_wasm() -> ExitStatus {
+    Command::new("cargo-cross-test")
+        .status()
+        .expect("Failed to cross-build against msvc")
+}
+
+// this does not work
+/*
+fn execute_test_with_xwin(target: &str) -> ExitStatus {
+    Command::new("cargo")
+        .args(["xwin", "test", "--tests", "--target", target,"--bin","test-cross-compile","--package","test-cross-compile"])
+        .status()
+        .expect("Failed to cross-build against msvc")
+}
+*/
+
+fn execute_test_default(target: &str) -> ExitStatus {
+    Command::new("cargo")
+        .args(["test", "--tests", "--target", target])
+        .status()
+        .expect("Failed to cross-build against msvc")
+}
+
+fn test(targets: &[Target]) {
+    let mut status;
+    let r_wasm = Regex::new(r"wasm.*").unwrap();
+    let r_windows = Regex::new(r".*windows.*").unwrap();
+    for target in targets.iter() {
+        println!("-=====================- {} [test] -=====================-", &target.triple);
+        status = match &target.triple {
+            val if r_wasm.is_match(val) => {
+                // this tool only work in a package directory
+                set_workspace("./test-cross-compile".into());
+                let t_status = execute_test_for_wasm();
+                set_workspace("..".into());
+                t_status
+            }
+            val if r_windows.is_match(val) => {
+                Command::new("echo")
+                    .args(["need to figure out how to run unittests for Windows binaries..."])
+                    .status()
+                    .expect("echo is missing!!")
+                // execute_test_with_xwin(target.triple.as_str())
+            }
+            _ => {
+                execute_test_default(target.triple.as_str())
+            }
+        };
+        if !status.success() {
+            std::process::exit(1);
+        }
+    }
+}
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     // Change directory to current user workspace folder. Currently hard-coded... 
     let home = env::var("HOME").unwrap_or(".".to_string()).to_owned();
     set_workspace(format!("{home}/workspace").into());
     execute_clippy();
     // Get all installed targets
-    let installed_targets = target::installed()
+    let installed_targets: &Vec<Target> = &target::installed()
         .expect("Failed to list installed targets");
-    println!("Installed targets:");
-    for target in installed_targets {
-        let mut status;
-        println!("=========\t{}\t========", &target.triple);
-        match target.triple.as_str() {
-            "wasm32-unknown-emscripten" => {
-                status = execute_build_with_emsdk(target.triple.as_str(), false);
-            }
-            "x86_64-pc-windows-msvc" => {
-                status = execute_build_with_xwin(target.triple.as_str(), false);
-            }
-            _ => {
-                status = execute_build_default(target.triple.as_str(), false);
-            }
-        }
-        if !status.success() {
-            std::process::exit(1);
-        }
-        match target.triple.as_str() {
-            "wasm32-unknown-emscripten" => {
-                status = execute_build_with_emsdk(target.triple.as_str(), true);
-            }
-            "x86_64-pc-windows-msvc" => {
-                status = execute_build_with_xwin(target.triple.as_str(), true);
-            }
-            _ => {
-                status = execute_build_default(target.triple.as_str(), true);
-            }
-        }
-
-        if !status.success() {
-            std::process::exit(1);
-        }
-    }
+    build(installed_targets, false);
+    test(installed_targets);
+    build(installed_targets, true);
     Ok(())
 }
